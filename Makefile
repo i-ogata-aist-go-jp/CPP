90f3746b768fd110fab48600491c9a4fa6d63de7
ichiro ogata <i.ogata@aist.go.jp>
2021-01-18 15:43:52 +0900

    clean up 3

--- Makefile
+++ Makefile
@@ -1,15 +1,11 @@
-tso:	tso.c
-	 g++ -S -std=c++2a tso.c
-	 as -o tso.o tso.s
-	 g++ -pthread -o tso tso.o
-	 ./tso
-ars:	ars.c
-	 g++ -S -std=c++2a ars.c
-	 as -o ars.o ars.s
-	 g++ -pthread -o ars ars.o
-	 ./ars
-tmp:	tmp.c
-	 g++ -S -std=c++2a tmp.c
-	 as -o tmp.o tmp.s
-	 g++ -pthread -o tmp tmp.o
-	 ./tmp
+ARS = ars
+CC = g++ 
+OPTS = -std=c++2a
+SRC = ars.c
+
+$(ARS):	$(SRC)
+	 $(CC) $(OPTS) -pthread -o $(ARS) $(SRC)
+
+.PHONY: clean
+clean:
+	rm $(ARS)
--- ars.c
+++ ars.c
@@ -2,31 +2,26 @@
 #include <atomic>
 #include <thread>
 
-std::atomic<int> data = 0;
+std::atomic<int> data(0);
 std::atomic<bool> ready(false);
-constexpr int TIME_TO_SLEEP = 3000;
 
-void sender() {
-	data.store(42, std::memory_order_relaxed); // must be executed before next STORE
-	std::this_thread::sleep_for(std::chrono::milliseconds(TIME_TO_SLEEP));
+void producer() {
+	data.store(42, std::memory_order_relaxed); // must be executed before next "store release"
 	ready.store(true, std::memory_order_release);
 }
-void receiver() {
+void consumer() {
 	int r;
-	while (!ready.load(std::memory_order_release)) {
+	while (!ready.load(std::memory_order_acquire)) {
 	}
-	r = data.load(std::memory_order_relaxed);  // must be executed after previous LOAD
+	r = data.load(std::memory_order_relaxed);  // must be executed after previous "load acquire"
 	printf("data = %d\n",r);
 }
 
 int main()
 {
-	std::thread t1(sender);
-	std::thread t2(receiver);
+	std::thread t1(producer);
+	std::thread t2(consumer);
 
 	t1.join();
 	t2.join();
 }
-
-
-
